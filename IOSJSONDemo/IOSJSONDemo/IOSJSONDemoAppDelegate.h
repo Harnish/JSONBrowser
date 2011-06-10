0ecd3b510b5162578777f09896e9fbf57e0f8d10
Joseph Harnish <joseph.harnish@spectrum-health.org>
2011-06-10 17:06:30 -0400

    having issues.  Starting over.

---
+++ IOSJSONDemo/IOSJSONDemo/IOSJSONDemoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  IOSJSONDemoAppDelegate.h
+//  IOSJSONDemo
+//
+//  Created by Joseph Harnish on 6/10/11.
+//  Copyright 2011 Coder Joes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IOSJSONDemoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
---
+++ IOSJSONDemo/IOSJSONDemo/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  IOSJSONDemo
+//
+//  Created by Joseph Harnish on 6/10/11.
+//  Copyright 2011 Coder Joes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
